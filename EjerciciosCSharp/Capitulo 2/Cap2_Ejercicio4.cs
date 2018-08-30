c5e894deb88599a26472fe9073da1d31cf341d8e
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2018-08-30 10:28:48 -0400

    Agregando Cap2_Ejercicio4

---
+++ EjerciciosCSharp/Capitulo 2/Cap2_Ejercicio4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosCSharp.Capitulo_2
+{
+    class Cap2_Ejercicio4
+    {
+        static void Main(string[] args)
+        {
+            double celsius;
+            double fahrenheit;
+            string valor;
+            Console.WriteLine("Digite los gracos celcius: ");
+            valor = Console.ReadLine();
+            celsius = Convert.ToDouble(valor);
+
+            fahrenheit = (9 / 5 * celsius) + 32;
+
+            Console.WriteLine("{0} celcius = {1} fahrenheit", celsius, fahrenheit);
+            Console.Read();
+        }
+    }
+}
