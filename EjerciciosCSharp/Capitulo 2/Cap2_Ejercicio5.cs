4e232ddc7a9365a3f742ffb10be76d8484ec4c6b
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2018-08-30 10:29:36 -0400

    Cap2_Ejercicio5

---
+++ EjerciciosCSharp/Capitulo 2/Cap2_Ejercicio5.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosCSharp.Capitulo_2
+{
+    class Cap2_Ejercicio5
+    {
+        static void Main(string[] args)
+        {
+            double dolares;
+            double ceuros = 0,836084749;
+            string valor;
+            Console.WriteLine("Digitar Monto de dolares a convertir a Euros:");
+            valor = Console.ReadLine();
+            dolares = Convert.ToDouble(valor);
+            conversion = dolares * ceuros;
+
+            Console.WriteLine("{0} Dolares = {1} Euros", dolares, conversion);
+
+        }
+    }
+}
