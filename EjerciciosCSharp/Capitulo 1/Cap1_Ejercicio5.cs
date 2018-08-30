465b3229c6b3bced360e44bf47ee44c25e4fb0af
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2018-08-30 09:10:46 -0400

    Agregando Ejercicio 5 del Cap1

---
+++ EjerciciosCSharp/Capitulo 1/Cap1_Ejercicio5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosCSharp.Capitulo_1
+{
+    class Cap1_Ejercicio5
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Oscar Slater Jimenez Paula");
+            Console.WriteLine("Matricula: 2014-0492");
+            Console.WriteLine("Estudiante de Ingenieria en Sistemas");
+            Console.WriteLine("UCNE");
+            Console.Read();
+        }
+    }
+}
