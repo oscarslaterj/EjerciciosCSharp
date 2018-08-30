77101264dee6623d67338acfbae84d6b36242450
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2018-08-30 08:26:54 -0400

    Agregando carpetas para los capitulos, y agregando ejercicio 1 cap 1

---
+++ EjerciciosCSharp/Capitulo 1/Cap1_Ejercicio1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosCSharp
+{
+    public class Cap1_Ejercicio1
+    {
+        static void main(String[] args)
+        {
+            Console.WriteLine("Oscar Slater Jimenez Paula");
+            Console.Read();
+        }
+    }
+}
