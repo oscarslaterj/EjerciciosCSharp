498cc989f2ff18bea09b53d3f0560b7f39f6ec24
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2018-08-30 09:16:00 -0400

    Agregando Ejercicio 1 Cap 2

---
+++ EjerciciosCSharp/Capitulo 2/Cap2_Ejercicio1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosCSharp.Capitulo_2
+{
+    class Cap2_Ejercicio1
+    {
+        static void Main(string[] args)
+        {
+            float Nlados;
+            float Vlados;
+            string valor;
+
+            Console.WriteLine("Digite Los lados del angulo:");
+            valor = Console.ReadLine();
+            Nlados = Convert.ToSingle(valor);
+            Console.WriteLine("Digite el valor de los lados: ");
+            valor = Console.ReadLine();
+            Vlados = Convert.ToSingle(valor);
+            float perimetro = Nlados * Vlados;
+            Console.WriteLine("El perimetro es: {0} ", perimetro);
+            Console.Read();
+        }
+    }
+}
